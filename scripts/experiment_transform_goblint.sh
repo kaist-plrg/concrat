5408360a4da5cbaa07434db314c84c0575519726
Jaemin Hong <hjm0901@gmail.com>
2023-01-09 20:15:55 +0900

    add goblint experiment script

---
+++ scripts/experiment_transform_goblint.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+echo -e 'Succ\tProject'
+while read d; do
+  if [ -f "$d/a.xml" ]; then
+    td=`mktemp -d`
+    GOBLINT=yes ./scripts/test_dir.sh $d $td > /dev/null 2>&1
+    case "$?" in
+      0) SUC="yes" ;;
+      *) SUC="no" ;;
+    esac
+    rm -rf $td
+    echo $SUC $d | tr ' ' '\t'
+  fi
+done < scripts/list_all
