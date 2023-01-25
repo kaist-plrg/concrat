3bd30f68a3c3b8826592116920343b2c43f7213a
Jaemin Hong <hjm0901@gmail.com>
2023-01-25 17:31:54 +0900

    fix test scripts

--- scripts/test_bench_goblint_success.sh
+++ scripts/test_bench_goblint_success.sh
@@ -5,7 +5,7 @@ set -e
 cargo build --release
 
 while read d; do
-  if [ -d "$d" ]; then
+  if [ -d "$d" ] && [ -f "$d/a.xml" ]; then
     td=`mktemp -d`
     GOBLINT=yes ./scripts/test_dir.sh $d $td
     rm -rf $td
