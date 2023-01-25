fc49e7997c96b9a7f80905aede1a6fec2f46ef72
Jaemin Hong <hjm0901@gmail.com>
2023-01-25 21:55:27 +0900

    omit fixed during counting

--- scripts/experiment_count.sh
+++ scripts/experiment_count.sh
@@ -2,8 +2,10 @@
 
 echo -e 'C\tRust\tMutex\tRwlock\tSpin\tCond\tProject'
 while read d; do
-  C=`cloc --csv $d/main.c | tail -n 1 | tr ',' '\n' | tail -n 1`
-  RUST=`wc -l $d/main.rs | tr ' ' '\n' | head -n 1`
-  API=`api_counter $d/main.rs`
-  echo $C $RUST $API $(basename $d) | tr ' ' '\t'
+  if [[ $d != *"-fixed" ]]; then
+    C=`cloc --csv $d/main.c | tail -n 1 | tr ',' '\n' | tail -n 1`
+    RUST=`wc -l $d/main.rs | tr ' ' '\n' | head -n 1`
+    API=`api_counter $d/main.rs`
+    echo $C $RUST $API $(basename $d) | tr ' ' '\t'
+  fi
 done < scripts/list_all
